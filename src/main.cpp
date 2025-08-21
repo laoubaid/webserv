afc6f90613680164616b5820cba2a2ddac0101f9
laoubaid <abaidhossin@gmail.com>
2025-08-21 18:04:44 +0100

    fix git issue

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: laoubaid <laoubaid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 16:22:40 by laoubaid          #+#    #+#             */
-/*   Updated: 2025/08/21 17:23:26 by laoubaid         ###   ########.fr       */
+/*   Updated: 2025/08/21 17:46:45 by laoubaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ int main(int ac, char **av)
 		std::vector<serverConf> servers = get_config(av[1]).parser();
 		std::cout << "\033[32mConfiguration file parsed successfully!\033[0m" << std::endl;
 		
-		// webServ WS(servers);
+		webServ WS(servers);
 	
-		// WS.setup_epoll();
-		// WS.setup_servers();
-		// WS.run();
+		WS.setup_epoll();
+		WS.setup_servers();
+		WS.run();
 	} catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 		return 1;
